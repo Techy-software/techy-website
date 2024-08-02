aa59d9587ec7804d96112beedaf8ba80a7380c0c
ZiadNasr <ziadnasr988@gmail.com>
2024-08-02 20:35:44 +0300

    Added Horizontal Managment Reusable Component + 1 page of settings

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,10 +1,30 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import BlueButton from "../../reusable components/BlueButton/BlueButton";
 import HorizontalManagment from "../../reusable components/HorizontalMangment/HorizontalManagment";
+import InputField from "../../reusable components/InputField/InputField";
 import ProfilePicture from "../../reusable components/ProfilePicture/ProfilePicture";
 import WhiteCard from "../../reusable components/WhiteCard/WhiteCard";
 import Topbar from "../Topbar";
+import { useState } from "react";
 
 const Settings = () => {
+  const [nationality, setNationality] = useState("");
+  const basicInfo = [
+    { label: "First Name (English)", placeholder: "ex (Ahmed)" },
+    { label: "Middle Name (English)", placeholder: "ex (Kamal)" },
+    { label: "Last Name (English)", placeholder: "ex (Mealy)" },
+  ];
+  const nationalities = [
+    { value: "eg", label: "Egypt" },
+    { value: "ca", label: "Canada" },
+    { value: "mx", label: "Mexico" },
+    { value: "gb", label: "United Kingdom" },
+    { value: "fr", label: "France" },
+  ];
+
+  const handleChange = (event) => {
+    setNationality(event.target.value);
+  };
   return (
     <div>
       <Topbar />
@@ -17,14 +37,97 @@ const Settings = () => {
         </div>
         <div className="col-span-9">
           <ProfilePicture title="Profile Picture">
-            <hr />
-            <BlueButton
-              text="Cancel"
-              buttonStyle={"bg-white"}
-              textStyle={"text-blue-600"}
-            />
-            <BlueButton />
+            <hr className="mt-5" />
+            <div className="w-100 justify-end flex mt-5">
+              <BlueButton
+                text="Cancel"
+                buttonStyle={"bg-white me-5"}
+                textStyle={"text-blue-600"}
+              />
+              <BlueButton />
+            </div>
           </ProfilePicture>
+          <WhiteCard title="Basic info" style="mt-12">
+            <div className="grid grid-cols-12 gap-4 mt-7">
+              {basicInfo.map((item, index) => (
+                <div className="col-span-6" key={index}>
+                  <InputField
+                    type="text"
+                    label={item.label}
+                    placeholder={item.placeholder}
+                  />
+                </div>
+              ))}
+              <FormControl className="col-span-6">
+                <label className="formLabel">Natioanlity</label>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={nationality}
+                  onChange={handleChange}
+                  displayEmpty
+                  sx={{ height: "35px" }}
+                >
+                  <MenuItem value="">
+                    <em className="text-slate-400">Please Select</em>
+                  </MenuItem>
+                  {nationalities.map((nationality) => (
+                    <MenuItem key={nationality.value} value={nationality.value}>
+                      {nationality.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <div className="col-span-6">
+                <InputField
+                  label="ID"
+                  placeholder="ex (29831234667765432)"
+                  type="text"
+                />
+              </div>
+              <FormControl className="col-span-6">
+                <label className="formLabel">Marital Status</label>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={nationality}
+                  onChange={handleChange}
+                  displayEmpty
+                  sx={{ height: "35px" }}
+                >
+                  <MenuItem value="">
+                    <em className="text-slate-400">Please Select</em>
+                  </MenuItem>
+                  {nationalities.map((nationality) => (
+                    <MenuItem key={nationality.value} value={nationality.value}>
+                      {nationality.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <FormControl className="col-span-6">
+                <label className="formLabel">Gender</label>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={nationality}
+                  onChange={handleChange}
+                  displayEmpty
+                  sx={{ height: "35px" }}
+                >
+                  <MenuItem value="">
+                    <em className="text-slate-400">Please Select</em>
+                  </MenuItem>
+                  {nationalities.map((nationality) => (
+                    <MenuItem key={nationality.value} value={nationality.value}>
+                      {nationality.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+          </WhiteCard>
         </div>
       </div>
     </div>
--- src/reusable components/HorizontalMangment/HorizontalManagment.jsx
+++ src/reusable components/HorizontalMangment/HorizontalManagment.jsx
@@ -10,38 +10,41 @@ const HorizontalManagment = (props) => {
         <hr />
         <ul className="">
           {props.steps.map((step, index) => (
-            <div
-              key={step}
-              className={`flex h-14 items-center text-slate-400 w-full cursor-pointer justify-between ${
-                props.currentStep === index ? "bg-white" : ""
-              }`}
-            >
-              <div className="flex items-center">
-                <div
-                  className={`me-3 px-0.5 h-14
+            <>
+              <div
+                key={step}
+                className={`flex h-14 items-center text-slate-400 w-full cursor-pointer justify-between ${
+                  props.currentStep === index ? "bg-white" : ""
+                }`}
+              >
+                <div className="flex items-center">
+                  <div
+                    className={`me-3 px-0.5 h-14
                     ${
                       props.currentStep === index
                         ? "active-item-horizontal-steps "
                         : "bg-white"
                     }`}
-                ></div>
-                <li
-                  key={index}
-                  className={`px-2 py-4 ${
-                    props.currentStep === index ? "text-black" : ""
+                  ></div>
+                  <li
+                    key={index}
+                    className={`px-2 py-4 ${
+                      props.currentStep === index ? "text-black" : ""
+                    }`}
+                  >
+                    {step}
+                  </li>
+                </div>
+                <span
+                  className={`mr-5 ${
+                    props.currentStep === index ? "text-black " : ""
                   }`}
                 >
-                  {step}
-                </li>
+                  {">"}
+                </span>
               </div>
-              <span
-                className={`mr-5 ${
-                  props.currentStep === index ? "text-black " : ""
-                }`}
-              >
-                {">"}
-              </span>
-            </div>
+              <hr />
+            </>
           ))}
         </ul>
       </div>
