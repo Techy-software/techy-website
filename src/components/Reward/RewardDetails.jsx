17ca6f7ed5a520d3c8e6f8e925458660927c3ad2
ZiadNasr <ziadnasr988@gmail.com>
2024-08-07 20:44:25 +0300

    Finished Reward Details

--- src/components/Reward/RewardDetails.jsx
+++ src/components/Reward/RewardDetails.jsx
@@ -2,7 +2,7 @@ import HorizontalMangment from "../../reusable components/HorizontalMangment/Hor
 import PostTopBar from "../../reusable components/PostTopBar/PostTopBar";
 import WhiteCard from "../../reusable components/WhiteCard/WhiteCard";
 import Topbar from "../Topbar";
-import PostImage from "../../assets/postImage.jpeg";
+import PostImage from "../../assets/reward.jpeg";
 const RewardDetails = () => {
   const steps = ["Overview", "users"];
   const specs = [
@@ -11,7 +11,9 @@ const RewardDetails = () => {
     { Title: "All courses discount" },
     { Description: "-" },
     { "Reward type": "Percentage" },
-    {},
+    { "Reward Value": "10%" },
+    { "Valid from": "01 Feb, 2024" },
+    { "Valid until": "05 Feb, 2024" },
   ];
   return (
     <div>
@@ -25,7 +27,34 @@ const RewardDetails = () => {
         <WhiteCard title="Overview" style={"col-span-9 p-5"}>
           <span className="text-slate-400">Thumbnail</span>
           <div className="flex justify-center">
-            <img src={PostImage} className="object-cover" />
+            <img
+              src={PostImage}
+              className="w-full h-96 object-cover rounded-xl"
+            />
+          </div>
+          <div className="flex">
+            <div>
+              {specs.map((spec) => {
+                return (
+                  <div className="flex my-4">
+                    <span className="text-slate-400">
+                      {Object.keys(spec)[0]}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="ms-9">
+              {specs.map((spec) => {
+                return (
+                  <div className="flex my-4">
+                    <div>
+                      <span>{Object.values(spec)[0]}</span>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </WhiteCard>
       </div>
