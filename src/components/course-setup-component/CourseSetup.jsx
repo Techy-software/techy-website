4134db531b1eeca73c1fbf91602d2457a8eacc00
MostafaGadoo <73221261+MostafaGadoo@users.noreply.github.com>
2025-05-13 23:47:59 +0300

    Done with course setup screens

--- src/components/course-setup-component/CourseSetup.jsx
+++ src/components/course-setup-component/CourseSetup.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import OverViewCourseSetup from "./OverViewCourseSetup";
-import CompletionCourseSetup from "./CompletionCourseSetup"
+import CompletionCourseSetup from "./CompletionCourseSetup";
+import CourseFAQSetup from "./FAQsCourseSetup";
+import CoursePaymentSetup from "./CoursePaymentSetup";
+import CoursePublishingSetup from "./CoursePublishingSetup";
 
 const CourseSetup = ({ currentStep, headerStep }) => {
   switch (currentStep) {
     case 0:
-      return <OverViewCourseSetup currentStep={currentStep} headerStep={headerStep} />;
+      return (
+        <OverViewCourseSetup
+          currentStep={currentStep}
+          headerStep={headerStep}
+        />
+      );
     case 1:
       return <CompletionCourseSetup currentStep={currentStep} />;
+    case 2:
+      return <CourseFAQSetup currentStep={currentStep} />;
+    case 3:
+      return <CoursePaymentSetup currentStep={currentStep} />;
+      case 4:
+        return <CoursePublishingSetup currentStep={currentStep}/> 
     default:
       return <p className="h-100 w-100 text-center">Invalid Page</p>;
   }
