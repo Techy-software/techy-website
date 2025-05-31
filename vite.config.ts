546e3b24159cd0f3a50e405b86ba51ab3266daf0
Amr Elsayed <aibrahim.e@mcst.edu.sa>
2025-05-31 18:02:33 +0300

    trail docker fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react(), svgr()],
   // server: {
   //   proxy: {
